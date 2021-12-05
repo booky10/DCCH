c61ec2c67e665215e5fe3586513b974d71f3606c
booky10 <boooky10@gmail.com>
2021-12-05 15:40:35 +0100

    Added required fabric mod classes and files

---
+++ src/main/java/tk/booky/dcch/DCCHClient.java
@@ -0,0 +1,11 @@
+package tk.booky.dcch;
+// Created by booky10 in DCCH (15:35 05.12.21)
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DCCHClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
